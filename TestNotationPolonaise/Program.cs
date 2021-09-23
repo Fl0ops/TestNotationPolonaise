90815022a161546a62cfea479bc9bfaecb6e4314
Champault Junin <junin.champault@gmail.com>
2021-09-23 22:17:53 +0200

    intégration du module de calcul d'un notation polonaise

--- TestNotationPolonaise/Program.cs
+++ TestNotationPolonaise/Program.cs
@@ -10,6 +10,76 @@ namespace TestNotationPolonaise
     class Program
     {
         /// <summary>
+        /// Donne le résultat d'une formule écrite en notation polonaise
+        /// </summary>
+        /// <param name="formule">formule en notation polonaise</param>
+        /// <returns>résultat</returns>
+        static Double Polonaise(String formule)
+        {
+            try
+            {
+                // Placement de la formule dans un tableau
+                string[] tab = formule.Split(' ');
+                int nbCases = tab.Length; // nbCases stock le nombre de case dans le tableau
+
+                // boucle tant qu'il ne reste pas qu'une seule case
+                while (nbCases > 1)
+                {
+                    // recherche d'un signe à partir de la fin
+                    int k = nbCases - 1;
+                    while (k > 0 && tab[k] != "+" && tab[k] != "-" && tab[k] != "*" && tab[k] != "/")
+                    {
+                        k--;
+                    }
+
+                    // récupération des 2 valeurs concernées par l'operateur 
+                    float a = float.Parse(tab[k + 1]);
+                    float b = float.Parse(tab[k + 2]);
+
+                    // calcul
+                    float result = 0;
+                    switch (tab[k])
+                    {
+                        case "+": result = a + b; break;
+                        case "-": result = a - b; break;
+                        case "*": result = a * b; break;
+                        case "/":
+                            // éviter la division par 0
+                            if (b == 0)
+                            {
+                                return Double.NaN;
+                            }
+                            result = a / b; break;
+                    }
+
+                    // stockage du résultat à la place du signe
+                    tab[k] = result.ToString();
+
+                    // Suppression des 2 cellules suivantes par décalage vers la gauche
+                    for (int j = k + 1; j < nbCases - 2; j++)
+                    {
+                        tab[j] = tab[j + 2];
+                    }
+                    // les cases suivantes sont mises à blanc
+                    for (int j = nbCases - 2; j < nbCases; j++)
+                    {
+                        tab[j] = " ";
+                    }
+                    nbCases = nbCases - 2;
+                }
+
+                // retour du résultat final
+                return Double.Parse(tab[0]);
+            }
+            catch
+            {
+                // erreur rencontrée
+                return Double.NaN;
+            }
+        }
+
+
+        /// <summary>
         /// saisie d'une réponse d'un caractère parmi 2
         /// </summary>
         /// <param name="message">message à afficher</param>
